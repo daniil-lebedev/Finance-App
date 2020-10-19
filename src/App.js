969c4ec68043fcd39a0214c4eb8a264ef6280726
Daniil Lebedev <daniilwork247@gmail.com>
2020-10-19 18:30:57 +0100

    Added theme switcher

--- src/App.js
+++ src/App.js
@@ -10,14 +10,32 @@ import { GlobalProvider } from './context/GlobalState';
 import './App.css';
 
 function App() {
+  const [darkMode, setDarkmode] = React.useState(false)
   return (
     <GlobalProvider>
       <Header />
-      <div className="container">
+      <div className={darkMode ? "dark-mode":"body"}>
+        <nav>
+          <div className="toggle-container">
+            <span className="toggle">
+              <input
+              checked={darkMode}
+              onChange={() => setDarkmode(prevMode => !prevMode)}
+              type="checkbox"
+              className="checkbox" 
+              id="checkbox"
+              />
+              <label htmlFor="checkbox" />
+            </span>
+          </div>
+        </nav>
+        <main>{darkMode ? "Dark Mode":"Light Mode"}</main>
+        <div className="container">
         <Balance />
         <IncomeExpenses />
         <TransactionList />
         <AddTransaction />
+      </div>
       </div>
     </GlobalProvider>
   );
